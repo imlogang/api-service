471296d2d6562ca4ca16ce65879d95068c9f25ae
Logan <37604784+imlogang@users.noreply.github.com>
2025-03-17 22:39:35 -0500

    try using pgx instead of sql

--- cmd/db/db.go
+++ cmd/db/db.go
@@ -1,10 +1,13 @@
 package db
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"log"
 	"os"
+
+	"github.com/jackc/pgx/"
 	_ "github.com/lib/pq"
 )
 
@@ -28,18 +31,20 @@ func LoadConfig() Config {
 
 func (c *Config) TestDBConnection() error {
 	// Connection string
-	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", 
-		c.Host, c.Port, c.User, c.Password, c.DB)
+	//connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", 
+	//	c.Host, c.Port, c.User, c.Password, c.DB)
+	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", c.User, c.Password, c.Host, c.Port, c.DB)
 
 	// Open a connection to the database
-	db, err := sql.Open("postgres", connStr)
+	//db, err := sql.Open("postgres", connStr)
+	conn, err := pgx.Connect(context.Background(), connStr)
 	if err != nil {
 		log.Fatal("Error opening connection to the database:", err)
 	}
-	defer db.Close()
+	defer conn.Close()
 
 	// Test the connection
-	err = db.Ping()
+	err = conn.Ping(context.Background())
 	if err != nil {
 		log.Fatal("Error connecting to the database:", err)
 	} else {
