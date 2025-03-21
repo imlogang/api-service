9c7be9a6998f6b0f07949fea9bae0e64c6bbac08
Logan <37604784+imlogang@users.noreply.github.com>
2025-03-21 00:23:14 -0500

    /docs/

--- cmd/main.go
+++ cmd/main.go
@@ -25,7 +25,7 @@ import (
 func main() {
 	r := chi.NewRouter()
 	r.Get("/swagger/*", httpSwagger.Handler(
-		httpSwagger.URL("http://go-api-service.go-api.svc.cluster.local:8080/swagger.json"),
+		httpSwagger.URL("http://go-api-service.go-api.svc.cluster.local:8080/docs/swagger.json"),
 	))
 
 	http.HandleFunc("/api/private/add_torrent", httpapi.AddTorrentHandler)
