5330aaecd9d74c569ebe66d8701b537c54b23be5
Logan <37604784+imlogang@users.noreply.github.com>
2025-03-26 01:19:52 -0500

    only update ID 1 from now on

--- cmd/db/db.go
+++ cmd/db/db.go
@@ -351,11 +351,12 @@ func PutAnswerInDB(tablenName string, answer string, column string, secondColumn
 		return "", fmt.Errorf("there was an error connecting to the database: %s", err)
 	}
 	defer DB.Close()
-	sql := fmt.Sprintf(`INSERT INTO %s ("ANSWER", "POSITION") VALUES ($1, $2) ON CONFLICT ("id") DO UPDATE SET "ANSWER" = $2, "POSITION" = $3;`, tablenName)
-	_, err = DB.Exec(sql, answer, numberInArray, numberInArray)
+	sql := fmt.Sprintf(`UPDATE %s SET "ANSWER" = $1, "POSITION" = $2 WHERE "ID" = 1;`, tablenName)
+	_, err = DB.Exec(sql, answer, numberInArray)
 	if err != nil {
 		return "", fmt.Errorf("there was an error updating the database: %s", err)
 	}
+	
 	
 	if err != nil {
 		log.Fatalf("there was an error updating the database: %s", err)
