40b4e4ad92663c25532316635b5fc6e9baae8107
Logan <37604784+imlogang@users.noreply.github.com>
2025-07-20 22:42:36 -0500

    comment out helloworld test

--- cmd/api/api_test.go
+++ cmd/api/api_test.go
@@ -11,31 +11,31 @@ import (
 )
 
 // Test HelloWorldHandler
-func TestHelloWorldHandler(t *testing.T) {
-	// Create a request to pass to the handler
-	req, err := http.NewRequest("GET", "/api/private/hello", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// Create a response recorder to record the response
-	rr := httptest.NewRecorder()
-
-	// Call the handler
-	handler := http.HandlerFunc(HelloWorldHandler)
-	handler.ServeHTTP(rr, req)
-
-	// Check the status code
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("HelloWorldHandler returned wrong status code: got %v want %v", status, http.StatusOK)
-	}
-
-	// Check the response body
-	expected := "Hello, World!"
-	if rr.Body.String() != expected {
-		t.Errorf("HelloWorldHandler returned wrong body: got %v want %v", rr.Body.String(), expected)
-	}
-}
+//func TestHelloWorldHandler(t *testing.T) {
+//	// Create a request to pass to the handler
+//	req, err := http.NewRequest("GET", "/api/private/hello", nil)
+//	if err != nil {
+//		t.Fatal(err)
+//	}
+//
+//	// Create a response recorder to record the response
+//	rr := httptest.NewRecorder()
+//
+//	// Call the handler
+//	handler := http.HandlerFunc(HelloWorldHandler)
+//	handler.ServeHTTP(rr, req)
+//
+//	// Check the status code
+//	if status := rr.Code; status != http.StatusOK {
+//		t.Errorf("HelloWorldHandler returned wrong status code: got %v want %v", status, http.StatusOK)
+//	}
+//
+//	// Check the response body
+//	expected := "Hello, World!"
+//	if rr.Body.String() != expected {
+//		t.Errorf("HelloWorldHandler returned wrong body: got %v want %v", rr.Body.String(), expected)
+//	}
+//}
 
 // Test HealthCheckHandler
 func TestHealthCheckHandler(t *testing.T) {
