293d2dccc6327901018b6287f6b336e436005a2f
imlogang <37604784+imlogang@users.noreply.github.com>
2025-07-23 16:11:38 -0500

    wire up listables test

--- cmd/api/api_test.go
+++ cmd/api/api_test.go
@@ -4,8 +4,10 @@ import (
 	"bytes"
 	"encoding/json"
 	"github.com/circleci/ex/testing/testcontext"
+	"golang.org/x/net/context/ctxhttp"
 	"gotest.tools/v3/assert"
 	"gotest.tools/v3/assert/cmp"
+	"net/http"
 	"net/http/httptest"
 	"net/url"
 	"testing"
@@ -87,46 +89,35 @@ func TestAPI_CreateTable(t *testing.T) {
 	}
 }
 
-//func TestAPI_ListTables(t *testing.T) {
-//	tests := []struct {
-//		name  string
-//		table string
-//	}{
-//		{
-//			name:  "Beemoviebot Table",
-//			table: "beemoviebot",
-//		},
-//		{
-//			name:  "Random Table",
-//			table: "random_table",
-//		},
-//	}
-//	for _, tt := range tests {
-//		tt := tt
-//		t.Run(tt.name, func(t *testing.T) {
-//			req := httptest.NewRequest("GET", "/api/private/list_tables", nil)
-//			w := httptest.NewRecorder()
-//			ListTablesAPI(w, req)
-//
-//			assert.Equal(t, http.StatusOK, w.Code)
-//			var tables []string
-//			err := json.NewDecoder(w.Body).Decode(&tables)
-//			if err != nil {
-//				t.Fatalf("Failed to decode response: %v", err)
-//			}
-//			for _, tt := range tests {
-//				found := false
-//				for _, table := range tables {
-//					if table == tt.table {
-//						found = true
-//						break
-//					}
-//				}
-//				if !found {
-//					t.Errorf("Table '%s' not found in response", tt.table)
-//				}
-//			}
-//		})
-//
-//	}
-//}
+func TestAPI_ListTables(t *testing.T) {
+	ctx := testcontext.Background()
+	tests := []struct {
+		name           string
+		expectedTables returnBody
+	}{
+		{
+			name:           "Return All Tables",
+			expectedTables: returnBody{Tables: []string{"beemoviebot", "random_table"}},
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			a, err := New(ctx)
+			assert.NilError(t, err)
+			w := httptest.NewRecorder()
+			u, err := url.Parse("http://localhost:8082/api/private/list_tables")
+			assert.NilError(t, err)
+
+			req := httptest.NewRequest("GET", u.String(), nil)
+			var tables []string
+
+			a.Router.ServeHTTP(w, req)
+
+			err := json.NewDecoder(w.Body).Decode(&tables)
+			assert.NilError(t, err)
+			assert.Check(t, cmp.DeepEqual(tables, tt.expectedTables))
+		})
+
+	}
+}
