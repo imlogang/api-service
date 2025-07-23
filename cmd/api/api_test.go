d78e4e7a9b6307603396a1982f6d5e9672eafec5
imlogang <37604784+imlogang@users.noreply.github.com>
2025-07-23 15:57:06 -0500

    try wiring up CreateTable test

--- cmd/api/api_test.go
+++ cmd/api/api_test.go
@@ -1,6 +1,7 @@
 package httpapi
 
 import (
+	"bytes"
 	"encoding/json"
 	"github.com/circleci/ex/testing/testcontext"
 	"gotest.tools/v3/assert"
@@ -43,46 +44,48 @@ func TestAPI_HelloWorldHandler(t *testing.T) {
 	}
 }
 
-//func TestAPI_CreateTable(t *testing.T) {
-//	tests := []struct {
-//		name    string
-//		request requestBody
-//	}{
-//		{
-//			name: "Beemoviebot Table",
-//			request: requestBody{
-//				TableName: "beemoviebot",
-//			},
-//		},
-//		{
-//			name: "Random Table",
-//			request: requestBody{
-//				TableName: "random_table",
-//			},
-//		},
-//	}
-//	for _, tt := range tests {
-//		tt := tt
-//		t.Run(tt.name, func(t *testing.T) {
-//			body, _ := json.Marshal(tt.request)
-//
-//			req := httptest.NewRequest("POST", "/api/private/create_table", bytes.NewReader(body))
-//			req.Header.Set("Content-Type", "application/json")
-//			w := httptest.NewRecorder()
-//			CreateTableAPI(w, req)
-//			assert.Equal(t, http.StatusOK, w.Code)
-//
-//			var response map[string]string
-//			err := json.NewDecoder(w.Body).Decode(&response)
-//			if err != nil {
-//				t.Fatalf("Failed to decode response: %v", err)
-//			}
-//			if _, exists := response["table_name_created"]; !exists {
-//				t.Errorf("Response missing 'table_name_created' key. Got: %v", response)
-//			}
-//		})
-//	}
-//}
+func TestAPI_CreateTable(t *testing.T) {
+	ctx := testcontext.Background()
+	tests := []struct {
+		name         string
+		request      requestBody
+		expectedResp returnBody
+	}{
+		{
+			name:         "Beemoviebot Table",
+			request:      requestBody{TableName: "beemoviebot"},
+			expectedResp: returnBody{TableCreated: "beemoviebot"},
+		},
+		{
+			name:         "Random Table",
+			request:      requestBody{TableName: "random_table"},
+			expectedResp: returnBody{TableCreated: "random_table"},
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Run(tt.name, func(t *testing.T) {
+				a, err := New(ctx)
+				assert.NilError(t, err)
+				w := httptest.NewRecorder()
+				u, err := url.Parse("localhost:8082/api/private/create_table")
+				assert.NilError(t, err)
+				body, err := json.Marshal(tt.request)
+				assert.NilError(t, err)
+
+				req := httptest.NewRequest("POST", u.String(), bytes.NewReader(body))
+				a.Router.ServeHTTP(w, req)
+
+				var resp returnBody
+				err = json.NewDecoder(w.Body).Decode(&resp)
+				assert.NilError(t, err)
+
+				assert.Check(t, cmp.DeepEqual(resp, tt.expectedResp))
+			})
+		})
+	}
+}
 
 //func TestAPI_ListTables(t *testing.T) {
 //	tests := []struct {
